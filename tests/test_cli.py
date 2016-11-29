69040fa0f54f509d51490aeb8ec059c8c0586e82
Thiago Paes <mrprompt@gmail.com>
2016-11-29 01:21:30 -0200

    utilizando exclusivamente variaveis de ambiente para os testes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,11 @@
+from os import environ
 from click.testing import CliRunner
-
 from checker.cli import cli
 
 
 def test_cli_count():
     runner = CliRunner()
-    result = runner.invoke(cli, ['mrprompt/CaixaEconomicaFederal', '1e32aa8d0fceec3eb73e9a2f8a837ebdab6ba6db'])
+    result = runner.invoke(cli, [environ['GITHUB_REPOSITORY'], environ['GITHUB_TOKEN']])
 
     assert result.exit_code == 0
     assert result.output == "passed\n"
