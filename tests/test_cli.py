8afaf272caa947b3cb60beddfcb4b208795f4a3f
Thiago Paes <mrprompt@gmail.com>
2016-11-29 01:30:53 -0200

    test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,15 @@
-from os import environ
+import os
 from click.testing import CliRunner
 from checker.cli import cli
 
 
 def test_cli_count():
     runner = CliRunner()
-    result = runner.invoke(cli, [environ['GITHUB_REPOSITORY'], environ['GITHUB_TOKEN']])
+
+    repository = os.environ.get('GITHUB_REPOSITORY')
+    token = os.environ.get('GITHUB_TOKEN')
+
+    result = runner.invoke(cli, [repository, token])
 
     assert result.exit_code == 0
     assert result.output == "passed\n"
