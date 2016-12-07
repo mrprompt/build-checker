a4acd48c0141ed67a01ea94dce32b0cb0a610e16
Thiago Paes <mrprompt@gmail.com>
2016-12-07 01:03:43 -0300

    fix tests on raspberry

--- checker/rpi.py
+++ checker/rpi.py
@@ -12,7 +12,7 @@ GPIO.setup(pins, GPIO.OUT)
 
 try:
     while True:
-        build = checker.cli()
+        build = checker()
 
         if build == 'failed':
             pin = pins[2]
--- tests/test_rpi.py
+++ tests/test_rpi.py
@@ -1,16 +1,15 @@
 import os
+
+from checker.cli import cli
+
 from click.testing import CliRunner
-from checker.rpi import rpi
 
-
-@pytest.mark.skip(reason="only works on real raspberry")
 def test_rpi():
     runner = CliRunner()
 
     repository = os.environ.get('GITHUB_REPOSITORY')
     token = os.environ.get('GITHUB_TOKEN')
 
-    result = runner.invoke(rpi, [repository, token])
+    result = runner.invoke(cli, [repository, token])
 
     assert result.exit_code == 0
-    assert result.output == "passed\n"
